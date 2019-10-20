35d8e85eeea05de683f236190d82e7d46dd19561
hassy511 <43968971+hassy511@users.noreply.github.com>
2019-10-20 22:58:16 +0500

    Assignment 2
    3 View Screen Updated

---
+++ Assignment2/app/src/main/java/com/example/assignment2/Home.java
@@ -0,0 +1,35 @@
+package com.example.assignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+    public void PersonalInfo(View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void Login(View v){
+        Intent intent = new Intent(this,Login.class);
+        startActivity(intent);
+    }
+
+    public void BeautyBeast(View v){
+        Intent intent = new Intent(this,BeautyAndBeast.class);
+        startActivity(intent);
+    }
+
+    public void PlaceView(View v){
+        Intent intent = new Intent(this,PlaceView.class);
+        startActivity(intent);
+    }
+}
---
+++ Assignment2/app/src/main/java/com/example/assignment2/Login.java
@@ -0,0 +1,15 @@
+package com.example.assignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
