1fa209a8b2305c0a31d6a826ec9f1f898abfa8e7
hassy511 <43968971+hassy511@users.noreply.github.com>
2019-10-27 01:02:25 +0500

    Assignment 4
    Assignment # 4 Complete Update

--- Assignment2/app/src/main/java/com/example/assignment2/MainActivity.java
+++ Assignment2/app/src/main/java/com/example/assignment2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -28,67 +29,70 @@ public class MainActivity extends AppCompatActivity {
         lastName = findViewById(R.id.txtLastName);
         gender = findViewById(R.id.txtGender);
         country = findViewById(R.id.txtCountry);
-        email = findViewById(R.id.txtemail);
-        password = findViewById(R.id.txtpassword);
+        email = findViewById(R.id.txtEmail);
+        password = findViewById(R.id.txtPassword);
         chkAgreement = findViewById(R.id.chkAgreement);
     }
 
     public void onClickNext(View v){
-        String fname = firstName.getText().toString();
-        String lname = lastName.getText().toString();
-        String genderText = gender.getText().toString();
-        String countryText = country.getText().toString();
-        String emailText = email.getText().toString();
-        String passwordText = password.getText().toString();
-        boolean isError= false;
 
-        if(fname != null && fname.equalsIgnoreCase("")){
-            firstName.setError("Please enter Valid First Name");
-            isError = true;
-        }
+            String fname = firstName.getText().toString();
+            String lname = lastName.getText().toString();
+            String genderText = gender.getText().toString();
+            String countryText = country.getText().toString();
+            String emailText = email.getText().toString();
+            String passwordText = password.getText().toString();
 
-        if(lname != null && lname.equalsIgnoreCase("")){
-            lastName.setError("Please enter Valid Last Name");
-            isError = true;
-        }
+            boolean isError= false;
 
-        if(genderText != null && genderText.equalsIgnoreCase("")){
-            gender.setError("Please enter Valid Gender");
-            isError = true;
-        }
+            if(fname != null && fname.equalsIgnoreCase("")){
+                firstName.setError("Please enter Valid First Name");
+                isError = true;
+            }
 
-        if(countryText != null && countryText.equalsIgnoreCase("")){
-            country.setError("Please enter Valid Country");
-            isError = true;
-        }
+            if(lname != null && lname.equalsIgnoreCase("")){
+                lastName.setError("Please enter Valid Last Name");
+                isError = true;
+            }
 
-        if(emailText != null && emailText.equalsIgnoreCase("")){
-            email.setError("Please enter Valid email");
-            isError = true;
-        }else if(!emailText.contains("@")){
-            email.setError("Please enter Valid email, @ is missing");
-            isError = true;
-        }else if(!emailText.contains(".com")){
-            email.setError("Please enter Valid email, .com is missing");
-            isError = true;
-        }
+            if(genderText != null && genderText.equalsIgnoreCase("")){
+                gender.setError("Please enter Valid Gender");
+                isError = true;
+            }
 
-        if(passwordText != null && passwordText.equalsIgnoreCase("")){
-            password.setError("Please enter Valid Password");
-            isError = true;
-        }else if(passwordText.length() < 5 || passwordText.length() > 8){
-            password.setError("Password lenght should be between 5-8 characters");
-            isError = true;
-        }
+            if(countryText != null && countryText.equalsIgnoreCase("")){
+                country.setError("Please enter Valid Country");
+                isError = true;
+            }
 
-        if(!chkAgreement.isChecked()){
-            chkAgreement.setTextColor(Color.RED);
-            isError = true;
-        }
+            if(emailText != null && emailText.equalsIgnoreCase("")){
+                email.setError("Please enter Valid email");
+                isError = true;
+            }else if(!emailText.contains("@")){
+                email.setError("Please enter Valid email, @ is missing");
+                isError = true;
+            }else if(!emailText.contains(".com")){
+                email.setError("Please enter Valid email, .com is missing");
+                isError = true;
+            }
 
-        if(!isError){
-            Intent intent = new Intent(this, PlaceView.class);
-            startActivity(intent);
-        }
+            if(passwordText != null && passwordText.equalsIgnoreCase("")){
+                password.setError("Please enter Valid Password");
+                isError = true;
+            }else if(passwordText.length() < 5 || passwordText.length() > 8){
+                password.setError("Password lenght should be between 5-8 characters");
+                isError = true;
+            }
+
+            if(!chkAgreement.isChecked()){
+                chkAgreement.setTextColor(Color.RED);
+                isError = true;
+            }
+
+            if(!isError){
+                Intent intent = new Intent(this, PlaceView.class);
+                startActivity(intent);
+            }
+
     }
 }
